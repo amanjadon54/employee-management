4c3e6cf84a5f9b82fab223d9c45a91f92db82353
amanjadon54 <amanjadon54@gmail.com>
2020-05-11 11:48:24 +0530

    Core: to string corrceted

--- src/main/java/com/ems/model/Employee.java
+++ src/main/java/com/ems/model/Employee.java
@@ -3,6 +3,7 @@ package com.ems.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
@@ -10,6 +11,7 @@ import java.time.LocalDateTime;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@ToString
 public class Employee implements Serializable {
 
     private int id;
@@ -17,16 +19,5 @@ public class Employee implements Serializable {
     private int age;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
-
-    @Override
-    public String toString() {
-        return "Employee{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", age=" + age +
-                ", createdAt=" + createdAt +
-                ", updatedAt=" + updatedAt +
-                '}';
-    }
 
 }
