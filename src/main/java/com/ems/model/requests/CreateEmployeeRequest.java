728addda3ab376f10cc3895c9f1f90ad4403fbdb
amanjadon54 <amanjadon54@gmail.com>
2020-05-11 11:47:04 +0530

    Core: recomputed the validation logic

--- src/main/java/com/ems/model/requests/CreateEmployeeRequest.java
+++ src/main/java/com/ems/model/requests/CreateEmployeeRequest.java
@@ -4,8 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Data
 @AllArgsConstructor
@@ -13,14 +15,17 @@ import javax.validation.constraints.NotNull;
 public class CreateEmployeeRequest {
 
     @NotNull
+    @Size(min = 1, max = 30)
     private String name;
 
     @NotNull
-    @Min(1)
+    @Min(18)
+    @Max(100)
     private int age;
 
     @NotNull
     @Min(1)
+    @Max(Long.MAX_VALUE)
     private long salary;
 
     @Override
