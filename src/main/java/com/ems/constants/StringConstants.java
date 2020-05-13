162a788a04af8a65a2e3de518942de3245c12223
amanjadon54 <amanjadon54@gmail.com>
2020-05-13 10:47:31 +0530

    Core: exception handling added for valid

--- src/main/java/com/ems/constants/StringConstants.java
+++ src/main/java/com/ems/constants/StringConstants.java
@@ -6,4 +6,5 @@ public class StringConstants {
     public static final String NAME_REGEX = "\\d?$";
     public static final String VALID_NAME_REGEX = "^[a-zA-Z]*$";
     public static final String VALID_SEARCH_NAME_REGEX = "^[a-zA-Z0-9]*$";
+    public static final String NAME_AGE_EXCEPTION = "CONSTRAINT FAILED: name can only have alphabets, age should be in between 18 to 100.";
 }
--- src/main/java/com/ems/handler/RestExceptionHandler.java
+++ src/main/java/com/ems/handler/RestExceptionHandler.java
@@ -2,15 +2,20 @@ package com.ems.handler;
 
 import com.ems.exception.ApiError;
 import com.ems.exception.EmployeeManagementException;
+import org.slf4j.MDC;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.ConstraintViolationException;
+
+import static com.ems.constants.StringConstants.LOG_ID;
+import static com.ems.constants.StringConstants.NAME_AGE_EXCEPTION;
 
 @Order(Ordered.HIGHEST_PRECEDENCE)
 @RestControllerAdvice
@@ -21,6 +26,15 @@ public class RestExceptionHandler {
             ConstraintViolationException e, HttpServletRequest request) {
         ApiError apiError = new ApiError(HttpStatus.BAD_REQUEST, e.getConstraintViolations().toString(),
                 null);
+
+        return new ResponseEntity(apiError, apiError.getHttpStatus());
+    }
+
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiError> handleValidBeanExceptions(
+            MethodArgumentNotValidException e, HttpServletRequest request) {
+        ApiError apiError = new ApiError(HttpStatus.BAD_REQUEST, NAME_AGE_EXCEPTION,"");
 
         return new ResponseEntity(apiError, apiError.getHttpStatus());
     }
